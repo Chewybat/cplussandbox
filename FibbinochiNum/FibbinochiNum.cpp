67447a6cb7e2725f6e8e58e7d701d85aca2c9109
jsandov <jesus.sandoval1@pcc.edu>
2019-07-16 23:36:55 -0700

    Initial commit with blank cpp file for fibonocci numbers assignment

---
+++ FibbinochiNum/FibbinochiNum.cpp
@@ -0,0 +1,31 @@
+// FibbinochiNum.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{	
+	//Step1
+    // Prompt user for the first two numbers
+	//Previous1
+	//Previous2
+
+	//Step2
+	// Read (Input), the first two numbers into Previous1 and Previous2
+
+	//Step3 
+	//Output the first two Fibonacci numbers (Echo Input)
+
+	//step4
+	//Prompt the user for the position of the desired
+	//Fibonacci number
+
+	//step5
+	//Read position of the desired Fibonacci number
+	//into nthFibonacci
+
+	return 0;
+}
+
